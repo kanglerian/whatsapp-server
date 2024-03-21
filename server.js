24a935ba27f13216162614e5795d56c68b5f1140
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-03-21 13:55:10 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const { HOST_CLIENT, PORT, CLIENT } = process.env;
+const express = require('express');
+const qrcode = require('qrcode');
+const http = require('http');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+
+const { Server } = require('socket.io');
+const { Client, LocalAuth } = require('whatsapp-web.js')
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [`${HOST_CLIENT}`],
+    methods: ["GET", "POST"]
+  }
+});
+
+const client = new Client({
+  restartOnAuthFail: true,
+  authStrategy: new LocalAuth({
+    clientId: 'client'
+  }),
+  puppeteer: {
+    headless: true
+  }
+});
+
+client.once('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('qr', (qr) => {
+  qrcode.toDataURL(qr, (error, url) => {
+    io.emit('qrcode', url);
+  })
+});
+
+client.on('message', (message) => {
+  console.log(message.body);
+});
+
+client.initialize();
+
+app.use(cors());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use(express.urlencoded({
+  extended: true,
+  limit: '25mb'
+}));
+
+app.get('/', (req, res) => {
+  res.send(`Server Whatsapp Sender: ${CLIENT}`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+})
+
+server.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
