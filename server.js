86f0959df00196a41b14a715ac9827d7be469c7d
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-03-28 09:10:23 +0700

    env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const fs = require('fs');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const { Server } = require('socket.io');
 const { Client, NoAuth, MessageMedia } = require('whatsapp-web.js')
