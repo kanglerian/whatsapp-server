bc31c3cb3dfea3308d93976561f72affaebae523
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-04-15 18:15:15 +0700

    add cache web

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ const client = new Client({
   authStrategy: new NoAuth(),
   puppeteer: {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  }
+  },
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
 
 const db = new sqlite3.Database(dbPath, (error) => {
