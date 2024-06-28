b83a5afedf563b200012bcf2983eafc78582521a
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-06-28 11:35:12 +0700

    cache baru

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const client = new Client({
   webVersionCache: {
     type: "remote",
     remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html",
   },
 });
 
